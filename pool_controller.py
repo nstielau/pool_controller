4ad343c90e8ce90b5a1b97fbc1cc925e7a4fabc9
Nick Stielau <nick.stielau@gmail.com>
2022-08-31 10:54:59 -0700

    Initial button press.

--- pool_controller.py
+++ pool_controller.py
@@ -1,9 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
+BLINKER_PIN=8
+LED_PIN=10
+BUTTON_PIN=12
+
 # Configure the PIN # 8
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(8, GPIO.OUT)
+GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(BLINKER_PIN, GPIO.OUT)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setwarnings(False)
 
 # Blink Interval 
@@ -11,10 +17,19 @@ blink_interval = .5 #Time interval in Seconds
 
 # Blinker Loop
 while True:
- GPIO.output(8, True)
+ GPIO.output(BLINKER_PIN, True)
  time.sleep(blink_interval)
- GPIO.output(8, False)
+ GPIO.output(BLINKER_PIN, False)
  time.sleep(blink_interval)
+
+  input_state = GPIO.input(BUTTON_PIN)
+  if input_state == False:
+    print('Button Pressed')
+    GPIO.output(LED_PIN, True)
+    time.sleep(0.2)
+  else
+    GPIO.output(LED_PIN, False)
+
 
 # Release Resources
 GPIO.cleanup()
