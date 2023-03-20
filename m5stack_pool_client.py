17e9fa1f152bee7d7eaaf484a551fc09d334e055
Nick Stielau <nick.stielau@gmail.com>
2023-03-20 13:58:00 -0700

    Updating m5UI.

--- m5stack_pool_client.py
+++ m5stack_pool_client.py
@@ -1,22 +1,25 @@
+import json
+import time
+
+import urequests
+
 from m5stack import *
 from m5ui import *
 from uiflow import *
-import urequests
-import time
-import json
 
+# Nice dark grey
 setScreenColor(0x222222)
 
+# FQDN for pool interface
 fqdn = "3da840e05d58dbccc1a52769e568811f.balena-devices.com"
 
-# Describe this function...
+# Query pool dta from server
 def getPoolData():
   lcd.clear()
   
-  label0 = M5TextBox(69, 67, "Status", lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
-  
+  label0 = M5TextBox(69, 67, "", lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
   title = M5Title(title="Pool Party", x=3, fgcolor=0xFFFFFF, bgcolor=0x0000FF)
-  status = M5TextBox(220, 2, "status", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+  status = M5TextBox(220, 2, "status", lcd.FONT_Default, 0xFFFFFF, rotate=0)  
   pool_rct = M5Rect(205, 54, 101, 150, 0x222222, 0xFFFFFF)
   spa_rct = M5Rect(205, 54, 50, 50, 0x222222, 0xFFFFFF)
   spa_lbl = M5TextBox(216, 77, "", lcd.FONT_Default, 0x000000, rotate=0)
@@ -28,23 +31,24 @@ def getPoolData():
     pool_data = json.loads(req.text)
     spa_on = pool_data["spa_heater_mode"]["state"]
     if spa_on:
-      spa_rct = M5Rect(205, 54, 50, 50, 0xFF0000, 0x000000)
       spa_temp = pool_data["current_spa_temperature"]["state"].split()[0]
+      spa_rct = M5Rect(205, 54, 50, 50, 0xFF0000, 0x000000)
       spa_lbl = M5TextBox(216, 77, spa_temp + " F", lcd.FONT_Default, 0x000000, rotate=0)
+      label0 = M5TextBox(5, 67, "Hot Tub:" + spa_temp + " F", lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
     else:
       spa_rct = M5Rect(205, 54, 50, 50, 0x0000FF, 0x000000)
       spa_lbl = M5TextBox(216, 77, "Off", lcd.FONT_Default, 0x000000, rotate=0)
     status.setText('Connected')    
   except:
-    label1.setText(str(req.text))
     status.setText('Error')
+    label0.setText(str(req.text))
 
+# Button listeners
 btnA.wasPressed(getPoolData)
 
-
+# Main loop
 spa_temp = 0
 getPoolData()
 while True:
   wait(300)
   getPoolData()
-
