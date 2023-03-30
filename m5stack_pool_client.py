8c428cc42c362d402e923636e21ba9d9bb812581
Nick Stielau <nick.stielau@gmail.com>
2023-03-29 20:13:35 -0700

    Updating the client.

--- m5stack_pool_client.py
+++ m5stack_pool_client.py
@@ -1,64 +1,87 @@
 import json
-import nvs
 import time
 
 import urequests
+import wifiCfg
 
 from m5stack import *
 from m5ui import *
 from uiflow import *
 
+wifiCfg.doConnect('SSID', 'password')
+
 # Nice dark grey
 setScreenColor(0x222222)
-
-# FQDN for pool interface
-fqdn = str(nvs.read_str('FQDN'))
-
-# Query pool dta from server
-def getPoolData():
-  lcd.clear()
-
-  label0 = M5TextBox(69, 67, "", lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
-  title = M5Title(title="Pool Party", x=3, fgcolor=0xFFFFFF, bgcolor=0x0000FF)
-  status = M5TextBox(220, 2, "status", lcd.FONT_Default, 0xFFFFFF, rotate=0)
-  pool_rct = M5Rect(205, 54, 101, 150, 0x222222, 0xFFFFFF)
-  spa_rct = M5Rect(205, 54, 50, 50, 0x222222, 0xFFFFFF)
-  spa_lbl = M5TextBox(210, 70, "", lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
-  btn_a_lbl = M5TextBox(35, 220, "Refresh", lcd.FONT_Default, 0xFFFFFF, rotate=0)
-
-  status.setText('Loading')
-  try:
-    req = urequests.request(
-      method='GET',
-      url='https://' + fqdn + '/pool',
-      headers={"Authentication": "Bearer " + str(nvs.read_str('TOKEN'))})
-    pool_data = json.loads(req.text)
-    spa_on = pool_data["spa_heater_mode"]["state"]
-    if spa_on:
-      spa_temp = pool_data["current_spa_temperature"]["state"].split()[0]
-      spa_rct = M5Rect(205, 54, 50, 50, 0xFF0000, 0x000000)
-      spa_lbl = M5TextBox(210, 70, spa_temp + " F", lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
-      label0 = M5TextBox(5, 67, "Hot Tub:" + spa_temp + " F", lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
-    else:
-      spa_rct = M5Rect(205, 54, 50, 50, 0x0000FF, 0x000000)
-      spa_lbl = M5TextBox(210, 70, "Off", lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
-    status.setText('Connected')
-  except:
-    status.setText('Error')
-    raise
-
-# Button listeners
-btnA.wasPressed(getPoolData)
-
 
 # Set initial vars
 # nvs.write(str('TOKEN'), 'MYTOKEN')
 # nvs.write(str('FQDN'), 'MYFQDN')
 # str(nvs.read_str('TOKEN'))
 
-# Main loop
+# FQDN for pool interface
+fqdn = "fq.dn"
+
+spa_on = False
 spa_temp = 0
-getPoolData()
+
+# Query pool dta from server
+def getPoolData():
+  global spa_on
+  global spa_temp
+  title = M5Title(title="Hot Tub Time Machine", x=3, fgcolor=0xFFFFFF, bgcolor=0x0000FF)
+  status = M5TextBox(220, 4, "Loading", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+  try:
+    req = urequests.request(
+      method='GET',
+      url='https://' + fqdn + '/pool')
+    pool_data = json.loads(req.text)
+    lcd.clear()
+    title = M5Title(title="Hot Tub Time Machine", x=3, fgcolor=0xFFFFFF, bgcolor=0x0000FF)
+    spa_on = pool_data["spa_heater_mode"]["state"]
+    if spa_on:
+      spa_temp = pool_data["current_spa_temperature"]["state"].split()[0]
+      btn_a_lbl = M5TextBox(35, 220, "Refresh", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+      label0 = M5TextBox(100, 70, spa_temp + " F", lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
+      thermTop = M5Rect(180, 40, 40, 150, 0xFF0000, 0xFF0000)
+      thermBall = M5Circle(200, 190, 30, 0xFF0000, 0xFF0000)
+      height = int(150/104*int(spa_temp))
+      thermTop2 = M5Rect(180, 40, 40, 150-height, 0x0000FF, 0x0000FF)
+    else:
+      title = M5Title(title="Hot Tub Time Machine", x=3, fgcolor=0xFFFFFF, bgcolor=0x0000FF)
+      btn_a_lbl = M5TextBox(35, 220, "Refresh", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+      spa_lbl = M5TextBox(100, 70, "Off", lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
+      thermTop = M5Rect(180, 40, 40, 150, 0x0000FF, 0x0000FF)
+      thermBall = M5Circle(200, 190, 30, 0x0000FF, 0x0000FF)
+      thermTop2 = M5Rect(180, 40, 40, 30, 0x0000FF, 0x0000FF)
+  except Exception as e:
+    status.setText('Error')
+    M5TextBox(20, 24, "FQDN: " + fqdn, lcd.FONT_Default, 0xFFFFFF, rotate=0)
+    M5TextBox(20, 64, "Error: " + str(e), lcd.FONT_Default, 0xFFFFFF, rotate=0)
+    M5TextBox(20, 104, "Type: " + str(e.__class__), lcd.FONT_Default, 0xFFFFFF, rotate=0)
+    raise e
+
+# Button listeners
+btnA.wasPressed(getPoolData)
+
+# Main loop
+wait(3)
+
 while True:
-  wait(300)
   getPoolData()
+  if spa_on and int(spa_temp) > 97:
+    for s in range(150):
+      R = 255
+      G = 0
+      B = 0
+      for i in range(256):
+        rgb.setColorFrom(1, 10, (R << 16) | (G << 8) | B)
+        rgb.setBrightness(i)
+        wait_ms(2)
+      for i in range(255, -1, -1):
+        rgb.setColorFrom(1, 10, (R << 16) | (G << 8) | B)
+        rgb.setBrightness(i)
+        wait_ms(2)
+  else:
+    wait(600)
+
+
